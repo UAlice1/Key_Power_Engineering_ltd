d7aaf208eabe0cdf5ccec6842f66ddcd25bbf2cf
UAlice1 <umubyeyialice7@gmail.com>
2025-08-20 21:46:55 +0200

    my project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#007bff',
+          secondary: '#6c757d',
+        },
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
